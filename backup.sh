8c7a3a75259cfd7959590994cdd3146f6dabc418
Matthias Bilger <matthias@bilger.info>
2019-10-10 14:11:47 +0200

    output

--- backup.sh
+++ backup.sh
@@ -3,13 +3,17 @@
 BACKUP_DATE=$(date +"%Y-%m-%d_%H-%M")
 
 cd /backup
-tar cJf ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz ./*
+echo "make archive"
+tar -c --checkpoint=.1000 -Jf ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz ./*
 cd /
 
 RECIPIENT=$(echo "$GPG_RECIPIENT" | sed "s/,/ --recipient /")
 
-gpg --trust-model always --output ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --encrypt --recipient $RECIPIENT ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
+echo "encrypting"
+gpg --trust-model always --enable-progress-filter --output ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --encrypt --recipient $RECIPIENT ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz
 
+echo "uploading"
 aws s3 cp ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg s3://$S3_BUCKET_NAME/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg --storage-class STANDARD_IA
 rm ~/$S3_BUCKET_NAME$BACKUP_DATE.tar.xz.gpg
+echo "done"
